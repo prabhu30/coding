0775d064890172c023d39f6260c7afcea4356b7d
akhila-77 <68263452+akhila-77@users.noreply.github.com>
2021-04-26 20:24:59 +0530

    Update Butterfly.py

--- Python/Patterns/Question 6/Butterfly.py
+++ Python/Patterns/Question 6/Butterfly.py
@@ -1,1 +1,17 @@
+n=int(input())
+for i in range(n):
+    for j in range(n*2):
+        if(j<=i or (i+j)>=n*2-1):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+for i in range(n):
+    for j in range(n*2):
+        if((i+j<=n-1) or (j>=n+i)):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
 
