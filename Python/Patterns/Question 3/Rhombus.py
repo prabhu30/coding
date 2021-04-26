4fca8b493af1ff591acf40200b468b72ceee9ab7
akhila-77 <68263452+akhila-77@users.noreply.github.com>
2021-04-26 12:03:36 +0530

    Update Rhombus.py

--- Python/Patterns/Question 3/Rhombus.py
+++ Python/Patterns/Question 3/Rhombus.py
@@ -5,9 +5,7 @@ for i in range(n):
         print(" ",end="")
     x-=1
     for l in range(n):
-        if(i==0 or i==n-1):
-            print("*",end="")
-        elif(l==0 or l==n-1):
+        if(i==0 or i==n-1 or l==0 or l==n-1):
             print("*",end="")
         else:
             print(" ",end="")
