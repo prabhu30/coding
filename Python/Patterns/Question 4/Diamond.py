de419fd81b02a95e2b6edd222d29637acb15db2d
akhila-77 <68263452+akhila-77@users.noreply.github.com>
2021-04-26 19:58:20 +0530

    Create Diamond.py

---
+++ Python/Patterns/Question 4/Diamond.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(n-1):
+    for k in range(i+1):
+        print(" ",end="")
+    for j in range(n-i-1):
+        print("*",end=" ")
+    print()
