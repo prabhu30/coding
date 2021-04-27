972f80d29ab69530248264fd06bf95b37a73ce76
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2021-04-27 20:56:14 +0530

    Update solution.py

--- Python/Hackerrank/18 _ Designer Door Mat/solution.py
+++ Python/Hackerrank/18 _ Designer Door Mat/solution.py
@@ -1,1 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a,b = map(int, input().split())
+s2 = ".|."
+s1 = "-"
+c = b-3
+d = 1
+for i in range(1,(a+1)//2):
+    print(s1*int(c/2)+s2*d+s1*int(c/2))
+    if i== ((a+1)//2)-1:
+        break
+    c = c - ((b/a)*2)
+    d += 2
+e = (b-7)//2
+print("-"*e+"WELCOME"+"-"*e)
+for i in range((a//2)+1,a):
+    print(s1*int(c/2)+s2*d+s1*int(c/2))
+    c = c + ((b/a)*2)
+    d -= 2
 
