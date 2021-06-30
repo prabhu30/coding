29c7ef415c243f8335c11e430f9c67004412578b
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2021-06-30 11:42:57 +0530

    Delete Python/Patterns directory

--- Python/Patterns/Question 1/V-Shape.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-for i in range(0,n//2+1):
-    for j in range(0,n):
-        if(i==j or (i+j)==(n-1)):
-            print(j+1,end="")
-        else:
-            print(" ",end="")
-    print()
--- Python/Patterns/Question 2/Pattern.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-for i in range(n):
-    for j in range(n*2):
-        if(j<=i or (i+j)>=(n*2-1)):
-            print("*",end="")
-        else:
-            print("#",end="")
-    print()
