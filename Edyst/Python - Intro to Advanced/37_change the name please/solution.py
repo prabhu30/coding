0882b9cd03bdb0ce43a9fab8e669aa5e1b34d86e
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2021-06-30 14:20:56 +0530

    Create solution.py

---
+++ Edyst/Python - Intro to Advanced/37_change the name please/solution.py
@@ -0,0 +1,9 @@
+def nameChanger(original,substitute):
+    original=original.replace(substitute,'%')
+    print(original)
+    
+# Your code below this line #
+
+nameChanger("vowel","l")
+nameChanger("superhero","r")
+nameChanger("Java","a")
