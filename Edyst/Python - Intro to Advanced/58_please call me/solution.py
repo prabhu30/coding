acfc3e76a4d53df06ded3ac178f3a5e422295317
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2021-06-30 23:56:33 +0530

    Create solution.py

---
+++ Edyst/Python - Intro to Advanced/58_please call me/solution.py
@@ -0,0 +1,12 @@
+def solve(morning,mother,sleep):
+    # write your code from here
+    if sleep==True:
+        print("False")
+    else:
+        if morning==True:
+            if mother==True:
+                print("True")
+            else:
+                print("False")
+        else:
+            print("True")
